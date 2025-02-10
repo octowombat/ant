34b2eb9b1848f0fc9818bcbeeb0e09ee2611821a
Willy Wombat <octowombat@gmail.com>
2025-02-10 15:22:28 +0000

    chore: version bump
    Signed-off-by: Willy Wombat <octowombat@gmail.com>

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Ant.MixProject do
       package: package(),
       name: "Ant",
       description: "Background job processing library for Elixir focused on simplicity",
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
